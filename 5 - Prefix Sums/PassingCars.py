5f81cc60c0ef65671bb06b7a9161d05232925254
drberg1000 <drberg1000@gmail.com>
2022-08-12 18:09:34 -0500

    And I thought I might get this on the first try...

--- 5 - Prefix Sums/PassingCars.py
+++ 5 - Prefix Sums/PassingCars.py
@@ -1,7 +1,10 @@
 """
 https://app.codility.com/programmers/lessons/5-prefix_sums/
 https://codility.com/media/train/3-PrefixSums.pdf
-https://app.codility.com/demo/results/trainingCXWPGS-RKQ/
+https://app.codility.com/demo/results/trainingCXWPGS-RKQ/  # Wasn't sure what's wrong here.  Suspected problems with solution_1() functions.
+https://app.codility.com/demo/results/trainingZ2FGJZ-RC6/  # Failed to use the prefix_sums() function.
+https://app.codility.com/demo/results/training68T7U2-WCV/  # Realized it should be suffix_sums(). How is this still O(N**2)?
+https://app.codility.com/demo/results/trainingWMZTTY-W8K/
 """
 
 """ 
@@ -37,16 +40,16 @@ def solution_2(a):
     return passing_car_count
 
 
-def solution(a):
+def solution_3(a):
     passing_car_count=0
 
-    prefix_sum = calculate_prefix_sums(a)
-    passing_car_count = calculate_passing_car_count(a, prefix_sum)
+    prefix_sum = calculate_prefix_sums_3(a)
+    passing_car_count = calculate_passing_car_count_3(a, prefix_sum)
 
     return passing_car_count
 
 
-def calculate_prefix_sums(array):
+def calculate_prefix_sums_3(array):
     prefix_sums = list()
     prefix_sums.append(array[0])
     for element in array[1:]:
@@ -55,7 +58,7 @@ def calculate_prefix_sums(array):
     return prefix_sums
 
 
-def calculate_passing_car_count(array, prefix_sums):
+def calculate_passing_car_count_3(array, prefix_sums):
     """ DOH!!! I didn't update this to use prefix_sums! """
     passing_car_count = 0
     for position, direction_one in enumerate(array, 1):
@@ -65,3 +68,67 @@ def calculate_passing_car_count(array, prefix_sums):
         passing_car_count += sum(array[position:])
 
     return passing_car_count
+
+
+""" SECOND ATTEMPT """
+" Same as the first, I suspected problems with the extra functions. "
+
+
+""" THIRD ATTEMPT """
+
+def solution_4(a):
+    suffix_sum = calculate_suffix_sums_4(a)
+    passing_car_count = calculate_passing_car_count_4(a, suffix_sum)
+
+    return passing_car_count
+
+
+def calculate_suffix_sums_4(array):
+    suffix_sums = []
+    suffix_sums.insert(0, array[-1])
+
+    for element in reversed(array[:-1]):
+        suffix_sums.insert(0, suffix_sums[0] + element)
+
+    return suffix_sums
+
+
+def calculate_passing_car_count_4(array, suffix_sums):
+    passing_car_count = 0
+    for position, direction_one in enumerate(array):
+        if direction_one == 1:
+            continue
+        passing_car_count += suffix_sums[position]
+
+    return passing_car_count
+
+
+"""" Penultimate """
+
+
+def solution_5(a):
+    suffix_sum = calculate_suffix_sums_5(a)
+    passing_car_count = calculate_passing_car_count_5(a, suffix_sum)
+
+    return passing_car_count
+
+
+def calculate_suffix_sums_5(array):
+    suffix_sums = [array[-1]]
+
+    for element in reversed(array[:-1]):
+        suffix_sums.append(suffix_sums[0] + element)
+
+    return list(reversed(suffix_sums))
+
+
+def calculate_passing_car_count_5(array, suffix_sums):
+    passing_car_count = 0
+    for position, direction_one in enumerate(array):
+        if direction_one == 1:
+            continue
+        passing_car_count += suffix_sums[position]
+
+    return passing_car_count
+
+
